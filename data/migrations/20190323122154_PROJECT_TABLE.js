686c8a6867a0ffb7cbcbf32992e6b9119ca5f9ee
Trip4077 <bj2336@email.vccs.edu>
2019-03-23 12:33:09 -0400

    Schemas and Database Created, Migration Up To Date

--- data/migrations/20190323122154_PROJECT_TABLE.js
+++ data/migrations/20190323122154_PROJECT_TABLE.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('project', tbl => {
+        tbl.increments();
+        
+        tbl.string('name').notNullable();
+        tbl.string('description')
+
+        tbl.boolean('complete').notNullable().defaultTo(false);
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('project')
 };
