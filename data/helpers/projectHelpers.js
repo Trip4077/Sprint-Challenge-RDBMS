0e1b8f34e4063ede77242c648574c565068ec17b
Trip4077 <bj2336@email.vccs.edu>
2019-03-23 12:38:32 -0400

    Project Helpers File Created with Add Project Method

---
+++ data/helpers/projectHelpers.js
@@ -0,0 +1,13 @@
+const knex = require('knex');
+const knexConfig = require('../../knexfile');
+
+const db = knex(knexConfig.development);
+
+module.exports = {
+
+    addProject: project => {
+        return db.insert(project)
+                 .into('project');
+    }
+
+}
